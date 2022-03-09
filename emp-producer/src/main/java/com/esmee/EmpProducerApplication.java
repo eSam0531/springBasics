fba7dedd8b5eff4fef27163ada1130900bdb8830
eSam0531 <esama01@jaguar.tamu.edu>
2022-03-09 14:11:25 -0600

    Intro to Eureka server with consumer and producer

--- emp-producer/src/main/java/com/esmee/EmpProducerApplication.java
+++ emp-producer/src/main/java/com/esmee/EmpProducerApplication.java
@@ -2,8 +2,10 @@ package com.esmee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EmpProducerApplication {
 
 	public static void main(String[] args) {
